baff3fefcabdc44a6d97d0625717787e43b198a8
Hugh Sorby <h.sorby@auckland.ac.nz>
2023-11-08 16:23:26 +1300

    Set plugin location.

--- mapclientplugins/sdsprotocolstep/__init__.py
+++ mapclientplugins/sdsprotocolstep/__init__.py
@@ -6,7 +6,7 @@ MAP Client Plugin - Generated from MAP Client v0.20.0
 __version__ = '0.1.0'
 __author__ = 'Hugh Sorby'
 __stepname__ = 'SDSProtocol'
-__location__ = ''
+__location__ = 'https://github.com/mapclient-plugins/mapclientplugins.sdsprotocolstep'
 
 # import class that derives itself from the step mountpoint.
 from mapclientplugins.sdsprotocolstep import step
